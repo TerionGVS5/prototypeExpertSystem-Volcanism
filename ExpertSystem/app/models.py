9850c30b40e515f47bd837cb2ea2a6aed8b5592a
TerionGVS <Vveterokk@gmail.com>
2018-03-17 13:02:19 +1000

    Добавление модели вулкана

--- ExpertSystem/app/models.py
+++ ExpertSystem/app/models.py
@@ -5,3 +5,9 @@ Definition of models.
 from django.db import models
 
 # Create your models here.
+class Volcano(models.Model):
+    name = models.CharField(max_length=50)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    def __str__(self):
+        return self.name
